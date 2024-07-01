d5503169b6acdc2735924d5d0c1f1ba28864f3cf
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-07-01 23:18:51 +0600

    submit

---
+++ object.js
@@ -0,0 +1,10 @@
+const parson = {
+    name : "Jahangir alam",
+    roll : 32,
+    great : function () {
+        return this.roll;
+        // console.log(this.roll);
+    },
+};
+// parson.great();
+console.log(parson.great());
