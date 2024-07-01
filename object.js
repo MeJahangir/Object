fdb2fc91cab90b11d68b3dd1c0d043c92efd56e8
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-07-01 23:26:46 +0600

    push

--- object.js
+++ object.js
@@ -8,3 +8,18 @@ const parson = {
 };
 // parson.great();
 console.log(parson.great());
+
+const person = {
+    name: "Jahangir Alam",
+    roll: 32,
+    greet: function() {
+        console.log("Hello, " + this.name);
+    }
+};
+
+// Accessing object properties
+console.log(person.name);  // Output: Jahangir Alam
+console.log(person.roll);  // Output: 32
+
+// Calling an object method
+person.greet();  // Output: Hello, Jahangir Alam
